817fd2cb3efaab6088ceac8518a7b4730a99cf64
gumguard <gawonlee0712@gmail.com>
2025-11-03 23:04:56 +0900

    feat(constants): 상수 객체 생성

---
+++ src/main/java/lotto/constant/Constants.java
@@ -0,0 +1,15 @@
+package lotto.constant;
+
+public class Constants {
+    private Constants() {}
+
+    // Lotto
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int LOTTO_MIN_NUMBER = 1;
+    public static final int LOTTO_MAX_NUMBER = 45;
+    public static final int WINNING_NUMBER_COUNT = 6;
+
+    // Format
+    public static final String ERROR_PREFIX = "[ERROR] ";
+}
